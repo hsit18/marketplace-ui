3a1b4142f7f28d1e9819a2c5b07538eb11c05b20
Harpreet Singh <hsit18@gmail.com>
2021-09-08 00:19:39 +0530

    search bar added

--- src/MarketPlace/index.js
+++ src/MarketPlace/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import SearchIcon from '@material-ui/icons/Search';
 
 import ProductCard from './ProductCard';
 
@@ -10,8 +13,17 @@ const MarketPlace = () => {
                 <Typography variant="body1">
                     MarketPlace / All Products
                 </Typography>
-
             </div>
+            <div className="product-search">
+                <TextField id="outlined-basic" label="" variant="outlined" size="small" fullWidth InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon />
+                        </InputAdornment>
+                    ),
+                }} />
+            </div>
+
             <div className="product-list">
                 <ProductCard card={{}} />
 
